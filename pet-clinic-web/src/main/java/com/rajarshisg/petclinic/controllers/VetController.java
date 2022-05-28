6ead0b2b11040d7fc9aa06f80298a8cf8511d1e9
Rajarshi Sengupta <rajarshisengupta@Rajarshis-MacBook-Air.local>
2022-05-29 02:04:46 +0530

    Add Owners and Vets List.

--- pet-clinic-web/src/main/java/com/rajarshisg/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/rajarshisg/petclinic/controllers/VetController.java
@@ -1,13 +1,22 @@
 package com.rajarshisg.petclinic.controllers;
 
+import com.rajarshisg.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping({"vets"})
 public class VetController {
+    VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/", "index", "index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
