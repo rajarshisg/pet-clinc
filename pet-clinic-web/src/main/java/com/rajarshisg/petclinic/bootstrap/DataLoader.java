edd9749309c36d61452b9f291eec079fd20ae13e
Rajarshi Sengupta <rajarshisengupta@Rajarshis-MacBook-Air.local>
2022-05-29 02:15:57 +0530

    Added Auto ID Generation for MapService.

--- pet-clinic-web/src/main/java/com/rajarshisg/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/rajarshisg/petclinic/bootstrap/DataLoader.java
@@ -23,25 +23,21 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Rajarshi");
         owner1.setLastName("Sengupta");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Pratyayee Deb");
         owner2.setLastName("Purkayastha");
         ownerService.save(owner2);
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Ross");
         vet1.setLastName("Geller");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Phoebe");
         vet2.setLastName("Buffay");
         vetService.save(vet2);
